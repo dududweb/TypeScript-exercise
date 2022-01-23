cd2e81be71c32442883f532a94f271f71826b3ff
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-23 16:02:46 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let project: {
+    member: string[];
+    days: number;
+    started: boolean;
+  } = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+  };
+  let user = "kim";
+  let age = undefined;
+  let married = false;
+  let 철수 = [user, age, married];
+
+  const returnName = (x?: string) => {
+    if (x) {
+      console.log("안녕하세요" + x);
+    } else {
+      console.log("입력바람");
+    }
+  };
+
+  const countNum = (x: number | string): number => {
+    return x.toString().length;
+  };
+
+  const currentCen = (
+    money: number,
+    home: boolean,
+    charming: string
+  ): string | void => {
+    let totalNum: number = 0;
+    totalNum += money;
+    if (charming === "상") {
+      totalNum += 100;
+    }
+    if (home === true) {
+      totalNum += 500;
+    }
+    if (totalNum >= 600) {
+      console.log("결혼가능");
+    }
+  };
+  currentCen(100, false, "상");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>count</span>
+      <span></span>
     </div>
   );
 }
